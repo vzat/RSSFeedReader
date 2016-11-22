58554387ec19bddb3f66cfecb5567e591ab39a6b
Vlad Zat <vlad.zat@mydit.ie>
2016-11-22 14:57:20 +0000

    Fixed Saving Articles

--- app/src/main/java/rss/feed/reader/rssfeedreader/SaveArticle.java
+++ app/src/main/java/rss/feed/reader/rssfeedreader/SaveArticle.java
@@ -1,56 +1,72 @@
 package rss.feed.reader.rssfeedreader;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class SaveArticle extends AppCompatActivity implements ListView.OnItemClickListener {
     Intent intent;
 
     ListView listView;
-    SimpleCursorAdapter adapter;
+//    SimpleCursorAdapter adapter;
+    CursorRadioAdapter adapter;
     DatabaseHelper db;
 
     int articleID;
     int savedDirectoryID;
+
+    private class CursorRadioAdapter extends CursorAdapter {
+        int savedDirectoryID;
+
+        public CursorRadioAdapter(Context context, Cursor cursor, int savedDirectoryID) {
+            super(context, cursor, 0);
+            this.savedDirectoryID = savedDirectoryID;
+        }
+
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            return LayoutInflater.from(context).inflate(R.layout.row_save_article, parent, false);
+        }
+
+        public void bindView(View view, Context context, Cursor cursor) {
+            TextView textView = (TextView) view.findViewById(R.id.savedDirectory);
+            textView.setText(cursor.getString(1));
+
+            if (savedDirectoryID != -1 && savedDirectoryID == cursor.getInt(0)) {
+                RadioButton radioButton = (RadioButton) view.findViewById(R.id.radio);
+                radioButton.setChecked(true);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_article);
 
-        // Set up listView
-        db = DatabaseHelper.getInstance(this);
-        listView = (ListView) findViewById(R.id.list);
-        adapter = new SimpleCursorAdapter(this, R.layout.row_save_article, db.getAllDirectories("Saved"), new String[] {"directoryName"}, new int[] {R.id.savedDirectory}, 0);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-
         // Set up initial values for the article and saved directory
+        db = DatabaseHelper.getInstance(this);
         intent = getIntent();
         articleID = intent.getIntExtra("articleID", -1);
         savedDirectoryID = db.getSavedDirectory(articleID);
         System.out.println(savedDirectoryID);
 
-        // If article is already saved then check that radio button
-        // *** MAKE A CUSTOM ADAPTER
-        if (savedDirectoryID != -1) {
-            Cursor cursor = adapter.getCursor();
-            int pos = 0;
-            while (cursor.moveToNext()) {
-                if (cursor.getInt(0) == savedDirectoryID)
-                    break;
-                pos++;
-            }
-            ((RadioButton)listView.getChildAt(pos)).setChecked(true);
-        }
-
+        // Set up listView
+        listView = (ListView) findViewById(R.id.list);
+//        adapter = new SimpleCursorAdapter(this, R.layout.row_save_article, db.getAllDirectories("Saved"), new String[] {"directoryName"}, new int[] {R.id.savedDirectory}, 0);
+        adapter = new CursorRadioAdapter(this, db.getAllDirectories("Saved"), savedDirectoryID);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
     }
 
     public void onItemClick(AdapterView l, View v, int position, long id) {
@@ -72,12 +88,17 @@ public class SaveArticle extends AppCompatActivity implements ListView.OnItemCli
         // Reference Complete
     }
 
-    public void onBackPressed() {
-        super.onBackPressed();
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//
+//        // Update the article in the database with the savedDirectory
+//        db.setSavedDirectory(articleID, savedDirectoryID);
+//
+//        finish();
+//    }
 
-        // Update the article in the database with the savedDirectory
+    public void onPause() {
+        super.onPause();
         db.setSavedDirectory(articleID, savedDirectoryID);
-
-        finish();
     }
 }
